b3007bc20967b312868fd5cac47fcfb453389252
bloprods <brunolopez25702blr@gmail.com>
2022-01-28 12:45:09 +0100

    Finalizado 100%

--- js/common.js
+++ js/common.js
@@ -7,6 +7,7 @@ function openNav() {
     Function to open side bar if it's close.
     */
     document.getElementById("openbtn").style.visibility = "hidden";
+    document.getElementById("sidebar").style.visibility = "visible";
     document.getElementById("sidebar").style.width = "350px";
     document.getElementById("sidebar").style.borderRightWidth = "5px";
     document.getElementById("container").style.marginLeft = "360px";
@@ -30,6 +31,32 @@ function closeNav() {
     document.getElementById("navigationMenu").style.marginLeft = "0px";
 }
 
-function submit() {
-    alert("Se ha enviado correctamente el formulario");
+function submit_form() {
+    var error = false;
+    
+    if (!document.getElementById("name").value) {
+        alert("ERROR: No se ha introducido el nombre.")
+        error = true;
+    } else if (!document.getElementById("apell").value) {
+        alert("ERROR: No se han introducido los apellidos.")
+        error = true;
+    }else if (!document.getElementById("dni").value) {
+        alert("ERROR: No se ha introducido el DNI.")
+        error = true;
+    }else if (!document.getElementById("fn").value) {
+        alert("ERROR: No se ha introducido la fecha de nacimiento.")
+        error = true;
+    }else if (!document.getElementById("mail").value) {
+        alert("ERROR: No se ha introducido el email.")
+        error = true;
+    }else if (!document.getElementById("cod").value) {
+        alert("ERROR: No se ha introducido el código postal.")
+        error = true;
+    }
+
+    if (!error) {
+        alert("Se ha enviado correctamente el formulario");
+    } 
+    
 }
+
